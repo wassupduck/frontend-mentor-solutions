416fe51ee0e5e0d14ed98e67995cdfa8c4e45a70
Jono Brogan <jonobrogan123@gmail.com>
2024-02-22 14:58:52 +0000

    A mostly working linaria stylelint setup
    - Linaria only supports upto stylelint 14 therefore we can't lint the tsx files.
    - Disables Linarias preprocessor and uses postcss instead.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import wyw from "@wyw-in-js/vite";
+import linaria from "@wyw-in-js/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/frontend-mentor-solutions/",
   plugins: [
     react(),
-    wyw({
-      include: ["**/*.{ts,tsx}"],
+    linaria({
+      include: ["src/**/*.{ts,tsx}"],
       babelOptions: {
         presets: ["@babel/preset-typescript", "@babel/preset-react"],
       },
+      preprocessor: "none",
     }),
   ],
   css: { modules: { localsConvention: "camelCaseOnly" } },
