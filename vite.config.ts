a1d97653fa5a7a70dbc4e131e0c1adc0c3fe2ff3
Jono Brogan <jonobrogan123@gmail.com>
2024-02-22 15:56:54 +0000

    linaria is crap

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import linaria from "@wyw-in-js/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/frontend-mentor-solutions/",
-  plugins: [
-    react(),
-    linaria({
-      include: ["src/**/*.{ts,tsx}"],
-      babelOptions: {
-        presets: ["@babel/preset-typescript", "@babel/preset-react"],
-      },
-      preprocessor: "none",
-    }),
-  ],
+  plugins: [react()],
   css: { modules: { localsConvention: "camelCaseOnly" } },
 });
