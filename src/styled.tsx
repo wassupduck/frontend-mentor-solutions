4e458e4fd5cdae11ff5d656b95875792ce57863a
Jono Brogan <jonobrogan123@gmail.com>
2024-03-01 10:27:20 +0000

    styled helper was a bad idea

--- src/styled.tsx
+++ src/styled.tsx
@@ -3,6 +3,19 @@ import React, { forwardRef } from "react";
 
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
 
+/* WARNING!
+
+This styled helper should NOT be used.
+
+Issues:
+- If two CSS rules have the same specificity
+  then the order of the rules in the stylesheet or HTML will
+  determine which declarations have priority. When using the styled helper
+  to extend a functional component that already has styles applied/has a className
+  the actual styles applied will be unpredicatable and determined by the order of the class rules
+  in the bundled CSS.
+
+*/
 function styled<
   T extends React.ElementType,
   P extends Partial<Omit<React.ComponentPropsWithoutRef<T>, "className">>
